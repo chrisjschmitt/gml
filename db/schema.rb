3eb6b2a3e42f07454c6518754e729e54c1097395
Chris Schmitt <chrisjschmitt@gmail.com>
2009-04-11 18:55:37 -0400

    Schema file updated to include url field in gift table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081216060041) do
+ActiveRecord::Schema.define(:version => 20090410160534) do
 
   create_table "exchanges", :force => true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20081216060041) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "messages", :force => true do |t|
